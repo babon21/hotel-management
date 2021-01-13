bddfbf85214a9d9342e7e67e3c84497a31468f9f
Дмитрий Даржаин <dimad_1996@mail.ru>
2021-01-14 01:07:55 +0300

    added booking postgres repo

---
+++ internal/booking/repository/postgres/postgres_room.go
@@ -0,0 +1,44 @@
+package postgres
+
+import (
+	"github.com/babon21/hotel-management/internal/booking/usecase"
+	"github.com/babon21/hotel-management/internal/domain"
+	"github.com/jmoiron/sqlx"
+)
+
+type postgresBookingRepository struct {
+	Conn *sqlx.DB
+}
+
+// NewPostgresBookingRepository will create an object that represent the BookingRepository interface
+func NewPostgresBookingRepository(conn *sqlx.DB) usecase.BookingRepository {
+	return &postgresBookingRepository{conn}
+}
+
+func (repo *postgresBookingRepository) CheckBookingExists(bookingId string) bool {
+	var booking domain.Booking
+	err := repo.Conn.Get(&booking, "SELECT * FROM booking WHERE id = $1", bookingId)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func (repo *postgresBookingRepository) GetList(roomId string) ([]domain.Booking, error) {
+	getListQuery := "SELECT * FROM booking WHERE room_id = $1 ORDER BY start_date ASC"
+	bookings := make([]domain.Booking, 0, 1)
+	err := repo.Conn.Select(&bookings, getListQuery, roomId)
+	return bookings, err
+}
+
+func (repo *postgresBookingRepository) Save(booking *domain.Booking) (string, error) {
+	var id string
+	err := repo.Conn.QueryRow("INSERT INTO booking(room_id, start_date, expiration_date) VALUES ($1, $2, $3) RETURNING id", booking.RoomId, booking.StartDate, booking.ExpirationDate).Scan(&id)
+	return id, err
+}
+
+func (repo *postgresBookingRepository) Remove(bookingId string) error {
+	deleteQuery := "DELETE FROM booking WHERE id = $1"
+	_, err := repo.Conn.Exec(deleteQuery, bookingId)
+	return err
+}
