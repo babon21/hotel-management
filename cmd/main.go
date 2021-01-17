202d1b48debb0f0aaa7064322e13afd8380520d7
Дмитрий Даржаин <dimad_1996@mail.ru>
2021-01-17 21:35:42 +0300

    update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,33 +2,30 @@ package main
 
 import (
 	"fmt"
+	"github.com/babon21/hotel-management/internal/http/middleware"
+	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo"
+
 	bookingRepository "github.com/babon21/hotel-management/internal/booking/repository/postgres"
 	bookingUseCase "github.com/babon21/hotel-management/internal/booking/usecase"
+	"github.com/babon21/hotel-management/internal/config"
 	roomRepository "github.com/babon21/hotel-management/internal/room/repository/postgres"
 	roomUseCase "github.com/babon21/hotel-management/internal/room/usecase"
-	"github.com/labstack/echo"
 	"github.com/rs/zerolog/log"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
-	"github.com/jmoiron/sqlx"
 
 	bookingHttp "github.com/babon21/hotel-management/internal/booking/delivery/http"
 	roomHttp "github.com/babon21/hotel-management/internal/room/delivery/http"
-	//"github.com/labstack/echo"
-	//"github.com/spf13/viper"
-	//"net/url"
-	//"time"
 )
 
 func main() {
-	dbUser := "postgres"
-	dbPassword := "postgres"
-	//host := "postgres"
-	host := "localhost"
-	dbPort := "5432"
-	dbName := "postgres"
+	conf := config.Init()
+	fmt.Println(conf.Server.Port)
+	//conf.Database.Host = "localhost"
 
-	connStr := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s", dbUser, dbPassword, host, dbPort, dbName)
+	connStr := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s", conf.Database.Username,
+		conf.Database.Password, conf.Database.Host, conf.Database.Port, conf.Database.DbName)
 	db, err := sqlx.Open("pgx", connStr)
 	if err != nil {
 		log.Fatal().Err(err).Msg(err.Error())
@@ -40,6 +37,8 @@ func main() {
 	}
 
 	e := echo.New()
+	middL := middleware.InitMiddleware()
+	e.Use(middL.AccessLogMiddleware)
 	roomRepo := roomRepository.NewPostgresRoomRepository(db)
 	roomUsecase := roomUseCase.NewRoomUsecase(roomRepo)
 	roomHttp.NewRoomHandler(e, roomUsecase)
@@ -48,5 +47,5 @@ func main() {
 	bookingUsecase := bookingUseCase.NewBookingUsecase(bookingRepo, roomRepo)
 	bookingHttp.NewBookingHandler(e, bookingUsecase)
 
-	log.Fatal().Msg(e.Start(":9090").Error())
+	log.Fatal().Msg(e.Start(":" + conf.Server.Port).Error())
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/babon21/hotel-management
 go 1.15
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
+	github.com/bxcodec/faker/v3 v3.5.0
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/jackc/pgx/v4 v4.10.1
 	github.com/jmoiron/sqlx v1.2.0
@@ -10,4 +12,5 @@ require (
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/rs/zerolog v1.20.0
 	github.com/spf13/viper v1.7.1
+	github.com/stretchr/testify v1.7.0
 )
