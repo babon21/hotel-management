16e4cf2b2c0f8ec1d81e79eaa6d6a65b565ffb71
Дмитрий Даржаин <dimad_1996@mail.ru>
2021-01-17 21:34:52 +0300

    update room logic

--- internal/domain/room.go
+++ internal/domain/room.go
@@ -2,7 +2,7 @@ package domain
 
 // Room ...
 type Room struct {
-	ID          int64  `json:"id"`
+	ID          string `json:"id"`
 	Price       string `json:"price"`
 	Description string `json:"desc"`
 	DateAdded   string `json:"date_added" db:"date_added"`
